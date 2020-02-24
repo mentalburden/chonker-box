071fc4ca0c2ec551559cd252a0649dc978defdb2
Nahta Realnaame <47120653+mentalburden@users.noreply.github.com>
2020-02-23 16:36:56 -0800

    Update wifite-individual.sh

--- wifite-individual.sh
+++ wifite-individual.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+###DANGER!!!###
+# USES UNSANITIZED ARG FROM INDEX.PHP
+# super dangerous and could be coupled with ;{} to break away from wifite and spawn nastyboi shells
+# need to santize input for mac addresses only in index.php!!!
+###DANGER!!!### @_@
 
 echo "<BR><BR><BR>started individual task for $1 -- <BR><BR><BR>"
 sudo /usr/sbin/wifite -i wlan1mon --pmkid --pmkid-timeout 30 --skip-crack -b $1
